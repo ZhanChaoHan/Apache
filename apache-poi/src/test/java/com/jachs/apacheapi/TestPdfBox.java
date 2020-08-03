569396bb01a0d67bac2ad6146541506d507000de
jachs <799516423@qq.com>
2020-08-03 16:57:31 +0800

    ccasa

--- apache-poi/src/test/java/com/jachs/apacheapi/TestPdfBox.java
+++ apache-poi/src/test/java/com/jachs/apacheapi/TestPdfBox.java
@@ -1,15 +1,3 @@
-/*
- * 文件名: TestPdfBox.java
- * 文件编号: 
- * 版权: Copyright (c) 2020, Sinosoft and/or its affiliates. All rights reserved.Use is subject to license terms.
- * 描述: TODO
- * 创建人: zhanchaohan
- * 创建时间: 2020年06月22日 11:04
- * 修改人:
- * 修改时间: 2020年06月22日 11:04
- * 修改变更号: 
- * 修改内容: TODO
- */
 package com.jachs.apacheapi;
 
 import java.io.IOException;
@@ -19,13 +7,7 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import org.junit.Test;
 
 /**
- * @Title TestPdfBox
- * @Description TODO
  * @author zhanchaohan
- * @date 2020年06月22日 11:04
- * @version V1.0
- * @see
- * @since V1.0
  * 
  */
 public class TestPdfBox {
