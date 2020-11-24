ffbc7e85fc5fc2b074201a18be8802a486d1f0b7
zhanchaohan <zhanchaohan@ZhanChaoHan>
2020-11-24 17:54:16 +0800

    QSSA

---
+++ Apache-poi/src/test/java/com/jachs/apacheapi/docx/TestDocx.java
@@ -0,0 +1,19 @@
+package com.jachs.apacheapi.docx;
+
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.junit.Test;
+
+/**
+ * @author zhanchaohan
+ * 
+ */
+public class TestDocx {
+    @Test
+    public void tests () throws IOException, OpenXML4JException {
+        XWPFDocument doc = new XWPFDocument ( TestDocx.class.getResourceAsStream ( "/docx/冲向云霄年终总结.docx" ) );
+        
+    }
+}
