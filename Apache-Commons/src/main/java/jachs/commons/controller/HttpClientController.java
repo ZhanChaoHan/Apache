7acf4b17e04e7085e38594110d552a520c2f8137
zhanchaohan <zhanchaohan@ZhanChaoHan>
2020-12-04 09:58:03 +0800

    vcze

---
+++ Apache-Commons/src/main/java/jachs/commons/controller/HttpClientController.java
@@ -0,0 +1,12 @@
+package jachs.commons.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author zhanchaohan
+ * 
+ */
+@Controller
+public class HttpClientController {
+
+}
