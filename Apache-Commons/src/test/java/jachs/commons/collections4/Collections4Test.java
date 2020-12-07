01dbbca477e3d619ba5b96bc29a710092378e406
799516423@qq.com <github_jachs22>
2020-12-07 21:34:36 +0800

    cbhaghuac

---
+++ Apache-Commons/src/test/java/jachs/commons/collections4/Collections4Test.java
@@ -0,0 +1,15 @@
+package jachs.commons.collections4;
+
+import org.junit.Test;
+
+/***
+ * 
+ * @author zhanchaohan
+ *@see https://commons.apache.org/proper/commons-collections/javadocs/api-4.4/index.html
+ */
+public class Collections4Test {
+	@Test
+	public void collections4Test() {
+		
+	}
+}
