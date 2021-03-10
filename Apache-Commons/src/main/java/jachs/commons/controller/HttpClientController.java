2c75af5bdc6b4e91c82070d9647d2027a21052f6
jachs <799516423@qq.com>
2021-03-10 17:26:52 +0800

    bbbbbbbbbbbssssssssss

--- Apache-Commons/src/main/java/jachs/commons/controller/HttpClientController.java
+++ Apache-Commons/src/main/java/jachs/commons/controller/HttpClientController.java
@@ -1,12 +1,27 @@
 package jachs.commons.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zhanchaohan
  * 
  */
-@Controller
+@RestController
 public class HttpClientController {
-
+    
+    @PostMapping( "/postT1" )
+    public String postT1 () {
+        return "postT1";
+    }
+    @GetMapping( "/getT1" )
+    public String getT1 () {
+        return "postT1";
+    }
+    @RequestMapping("/rm1")
+    public String rm1() {
+        return "rm1";
+    }
 }
