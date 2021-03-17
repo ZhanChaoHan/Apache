c46a179e8e248e2cd0191ea670f82005b0687449
jachs <799516423@qq.com>
2021-03-17 15:52:06 +0800

    ACZS

--- JDK/src/test/java/com/jachs/jdk/utill/list/collection/list/ArrayListTest.java
+++ JDK/src/test/java/com/jachs/jdk/utill/list/collection/list/ArrayListTest.java
@@ -1,10 +1,21 @@
 package com.jachs.jdk.utill.list.collection.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
 /***
- * 
+ * 底层数据结构是数组，查询快，增删慢，线程不安全，效率高，可以存储重复元素
  * @author zhanchaohan
  *
  */
 public class ArrayListTest {
-
+	List<String>sList=new ArrayList<String>();
+	List<Integer>iList=new ArrayList<Integer>();
+	
+	@Test
+	public void test() {
+		
+	}
 }
--- JDK/src/test/java/com/jachs/jdk/utill/list/collection/list/LinkedListTest.java
+++ JDK/src/test/java/com/jachs/jdk/utill/list/collection/list/LinkedListTest.java
@@ -1,7 +1,7 @@
 package com.jachs.jdk.utill.list.collection.list;
 
 /***
- * 
+ * 底层数据结构是链表，查询慢，增删快，线程不安全，效率高，可以存储重复元素
  * @author zhanchaohan
  *
  */
