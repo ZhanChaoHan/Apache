b1ebe9cb50aac4ac5f7dfc12b71181d5acc98196
jachs <799516423@qq.com>
2021-12-17 09:28:55 +0800

    注册表

---
+++ Registry/src/test/java/com/jachs/registry/PreferencesTest.java
@@ -0,0 +1,47 @@
+package com.jachs.registry;
+
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+import org.junit.Test;
+
+/***
+ * 
+ * @author zhanchaohan
+ *
+ */
+public class PreferencesTest {
+    String[] keys = { "version", "initial", "creator" };
+    String[] values = { "1.3", "ini.mp3", "799516423@qq.com" };
+ 
+    // 把相应的值储存到变量中去
+    @Test
+    public void writeValue() {
+        // HKEY_LOCAL_MACHINE\Software\JavaSoft\prefs下写入注册表值.
+        Preferences pre = Preferences.systemRoot().node("/javaplayer");
+        for (int i = 0; i < keys.length; i++) {
+            pre.put(keys[i], values[i]);
+        }
+    }
+ 
+    /***
+     * 根据key获取value
+     * 
+     */
+    @Test
+    public void getValue() {
+        Preferences pre = Preferences.systemRoot().node("/javaplayer");
+        System.out.println(pre.get("version", "time"));
+    }
+ 
+    /***
+     * 清除注册表
+     * 
+     * @throws BackingStoreException
+     */
+    @Test
+    public void clearValue() throws BackingStoreException {
+        Preferences pre = Preferences.systemRoot().node("/javaplayer");
+        pre.clear();
+    }
+}
